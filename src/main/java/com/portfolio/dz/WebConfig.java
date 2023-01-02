eb8b09d2b1e584c41ee19c41248d44df700c1049
Dante Zulli <dantezulli2004@gmail.com>
2023-01-02 16:25:26 -0300

    Clase WebConfig

--- src/main/java/com/portfolio/dz/WebConfig.java
+++ src/main/java/com/portfolio/dz/WebConfig.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.portfolio.dz;
+
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  *
- * @author dante
+ *  * Clase que habilita CORS
+ *
+ *  * @author YOProgramo
+ *
+ *  
  */
-public class WebConfig {
-    
+@EnableWebMvc
+
+@Configuration
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**");
+
+    }
+
 }
