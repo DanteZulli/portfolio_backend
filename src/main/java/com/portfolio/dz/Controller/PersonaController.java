489825f0d83fa7b0284cf1b74d0d822a82caeeff
Dante Zulli <dantezulli2004@gmail.com>
2023-01-02 16:21:13 -0300

    Clase para evitar CORS

--- src/main/java/com/portfolio/dz/Controller/PersonaController.java
+++ src/main/java/com/portfolio/dz/Controller/PersonaController.java
@@ -9,7 +9,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+//@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PersonaController {
 
     @Autowired
