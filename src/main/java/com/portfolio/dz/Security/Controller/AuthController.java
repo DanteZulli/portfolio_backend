291b744abcb4bc0cc90433aefd7592e7be8ab62d
Dante Zulli <dantezulli2004@gmail.com>
2023-01-02 13:09:03 -0300

    Update AuthController.java

--- src/main/java/com/portfolio/dz/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/dz/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
