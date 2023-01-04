95647d8ecc0b3b0a71ca7f40e3ffc1184e10207b
Dante Zulli <dantezulli2004@gmail.com>
2023-01-04 14:16:08 -0300

    CORS

--- src/main/java/com/portfolio/dz/Controller/ExperienciaController.java
+++ src/main/java/com/portfolio/dz/Controller/ExperienciaController.java
@@ -13,7 +13,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,7 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/explab")
-//@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ExperienciaController {
 
     @Autowired
